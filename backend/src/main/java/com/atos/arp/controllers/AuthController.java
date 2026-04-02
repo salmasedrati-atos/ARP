d2b0a39dc7a53f9a3f85bc188d992831d4590779
Salma <salma.sedrati@atos.net>
2026-04-02 11:52:21 +0100

    Correction des erreurs

--- backend/src/main/java/com/atos/arp/controllers/AuthController.java
+++ backend/src/main/java/com/atos/arp/controllers/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
