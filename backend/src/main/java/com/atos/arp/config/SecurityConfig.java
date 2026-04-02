a7f353dfb2e4580a70b71e9be81016f08e421929
Salma <salma.sedrati@atos.net>
2026-04-02 12:24:49 +0100

    Correction des erreurs

--- backend/src/main/java/com/atos/arp/config/SecurityConfig.java
+++ backend/src/main/java/com/atos/arp/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults()) 
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login").permitAll()
